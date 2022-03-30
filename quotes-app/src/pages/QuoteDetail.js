4ef66de4d39b5734afcab2a22ad9d5a4849fea7e
Ayaka Yasuda <ayakayasuda23@gmail.com>
2022-03-30 01:06:31 -0700

    added components relevant to quotes to pages and wrapped all pages with a layout component

--- quotes-app/src/pages/QuoteDetail.js
+++ quotes-app/src/pages/QuoteDetail.js
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Route, useParams } from "react-router-dom";
 import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Max", text: "Learning React is fun!" },
+  { id: "q2", author: "Jane", text: "Learning React is great!" },
+];
 
 const QuoteDetail = () => {
   const params = useParams();
 
+  const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No quote found!</p>;
+  }
+
   return (
-    <>
-      <div>Quote Detail Page</div>
-      <p>{params.quoteId}</p>
+    <Fragment>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
-    </>
+    </Fragment>
   );
 };
 
